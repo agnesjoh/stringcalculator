5a891d13169601290328c343569309b0d399fc4c
Agnes <agnesjoha@gmail.com>
2015-10-16 19:02:50 +0000

    add test case to check if two numbers works

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -18,4 +18,10 @@ public class CalculatorTest {
 	public void testOneNumber() {
 		assertEquals(1, Calculator.add("1"));
 	}
+
+	@Test
+	public void testTwoNumber() {
+		assertEquals(3, Calculator.add("1,2"));
+	}
+
 }
